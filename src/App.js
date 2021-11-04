4de60b03cfc5538459d3a00becf154e6768e055f
chandya <chandya.c99@gmail.com>
2021-11-04 20:49:43 +0800

    changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+      <h2>Author: Chandya</h2>
+    </div>
+  );
+}
+
+export default App;
